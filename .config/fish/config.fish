066e45539f0893ebb2b99dd2a33ef5274f90c507
Kevin <jimunbatty@gmail.com>
2023-07-06 12:13:15 -0700

    Add rofi config. Host-specific monitor configs.

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path --path ~/bin
+fish_add_path --path ~/.config/bin
+fish_add_path --path ~/.emacs.d/bin
